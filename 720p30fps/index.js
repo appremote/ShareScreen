9cdde4f1c5d608a1a59a64e6935221bec655a1a9
appremote <85788519+appremote@users.noreply.github.com>
2021-08-09 21:26:11 +0900

    Update index.js

--- 720p30fps/index.js
+++ 720p30fps/index.js
@@ -14,8 +14,8 @@ const mediaStreamConstraints = {
         channelCount: 2
   },
   video: {
-    //width: 720,
-    //height: 480, 
+    width: 720,
+    height: 480, 
     "frameRate": {"max": 30}
   } 
 };

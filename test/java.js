988cbd7711c1cc0b4da3d1c6285bcb61980bdb06
appremote <85788519+appremote@users.noreply.github.com>
2021-08-10 21:56:08 +0900

    Add files via upload

---
+++ test/java.js
@@ -0,0 +1,15 @@
+<script>
+
+var pw;
+
+pw = prompt("パスワードを入れて下さい。","");
+
+if (pw == "aaa" |pw == "ddd"){
+    location.href = "https://appremote.github.io/ShareScreen/720p30fps/";
+}else{
+    alert("パスワードが違います！");
+    location.href = "https://appremote.github.io/ShareScreen/72030fps/";
+
+}
+
+</script>
